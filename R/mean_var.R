d64499d291217bbebb57d0578caf7a9640814640
bsaul <bradleysaul@gmail.com>
2018-11-01 15:46:05 -0400

    adds n to mean_var for group sizes

--- R/mean_var.R
+++ R/mean_var.R
@@ -12,61 +12,61 @@ multinom_var <- function(p){
   diag(p) - outer(p, p)
 }
 
-#' Compute mean and variance
+#' Compute n, mean and variance
 #'
 #' Available for \code{numeric}, \code{integer}, and \code{factor} objects.
 #' \code{character} objects are handled by first converting to a factor.
 #'
-#' @name mean_var
+#' @name n_mean_var
 #' @param x a vector of values
 #' @importFrom stats var
 #' @return a list containing \code{mean} and \code{var}
 
-setGeneric("mean_var", def = function(x) {
-  standardGeneric("mean_var")
+setGeneric("n_mean_var", def = function(x) {
+  standardGeneric("n_mean_var")
 })
 
-#' @rdname mean_var
+#' @rdname n_mean_var
 
 setMethod(
-  f          = "mean_var",
+  f          = "n_mean_var",
   signature  = "numeric",
   definition = function(x){
-    list(mean = mean(x), var = stats::var(x))
+    list(n = length(x), mean = mean(x), var = stats::var(x))
 } )
 
 setMethod(
-  f          = "mean_var",
+  f          = "n_mean_var",
   signature  = "integer",
   definition = function(x){
 
     check_for_two_levels(x)
-    list(mean = mean(x), var = stats::var(x))
+    list(n = length(x), mean = mean(x), var = stats::var(x))
 } )
 
-#' @rdname mean_var
+#' @rdname n_mean_var
 
 setMethod(
-  f          = "mean_var",
+  f          = "n_mean_var",
   signature  = "logical",
   definition = function(x){
-    list(mean = mean(x), var = stats::var(x))
+    list(n = length(x), mean = mean(x), var = stats::var(x))
 } )
 
-#' @rdname mean_var
+#' @rdname n_mean_var
 
 setMethod(
-  f          = "mean_var",
+  f          = "n_mean_var",
   signature  = "factor",
   definition = function(x){
     p <- prop.table(table(x))
-    list(mean = p, var = multinom_var(p))
+    list(n = length(x), mean = p, var = multinom_var(p))
   })
 
-#' @rdname mean_var
+#' @rdname n_mean_var
 
 setMethod(
-  f          = "mean_var",
+  f          = "n_mean_var",
   signature  = "character",
   definition = function(x){
     x <- as.factor(x)
@@ -75,7 +75,7 @@ setMethod(
       warning("x has more than 50 levels. Are you sure you meant for this?")
     }
 
-    mean_var(x)
+    n_mean_var(x)
 })
 
 
