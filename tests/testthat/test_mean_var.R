a93bdd7b1f0aeb17226ae4ed4571f8f498576984
yingyu2017 <yingyu@novisci,.com>
2018-11-08 16:53:13 -0500

    finished rest of tests

--- tests/testthat/test_mean_var.R
+++ tests/testthat/test_mean_var.R
@@ -5,12 +5,12 @@ expect_list <- function(x){
 }
 
 ## TODO: tests to add
-# unsorted factors
-# missing x
-# length(0) x
-# length(1) x
-# factor with only one level
-# character with > 50 unique values
+# unsorted factors (done)
+# missing x (done)
+# length(0) x (done)
+# length(1) x (done)
+# factor with only one level (done)
+# character with > 50 unique values (serious?)
 
 test_that("n_mean_var works for numeric values", {
   expect_list(rnorm(10))
@@ -30,4 +30,30 @@ test_that("n_mean_var works for factor values", {
 
 test_that("n_mean_var works for character values", {
   expect_list(rep(c("x", "y", each = 4)))
+})
+
+test_that("n_mean_var works for unsorted factors", {
+  expect_list(factor(c(1,2,2,2,1,1)))
+})
+
+test_that("n_mean_var works for missing x", {
+  expect_list(c(1, 2, 3, NA))
+})
+
+# Error occured
+test_that("n_mean_var works for length(0) x", {
+  expect_list(c(NULL))
+})
+
+test_that("n_mean_var works for length(1) x", {
+  expect_list(1)
+})
+
+test_that("n_mean_var works for factor with only 1 level", {
+  expect_list(factor(c(1,1,1,1,1,1)))
+})
+
+x <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+test_that("n_mean_var works for 26 unique values", {
+  expect_list(x)
 })
